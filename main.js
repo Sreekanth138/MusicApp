3b03bf94f9c9194e831a0ae273a2ace71710fbfa
Sreekanth <kanthuachu@gmail.com>
2021-09-05 11:16:17 +0530

    updatingBar

--- main.js
+++ main.js
@@ -7,11 +7,17 @@ console.log(canvasWidth,canvasHeight)
 
 const canvasContext = canvas.getContext('2d');
 
+var audioEl = document.getElementById("audio")
+var ctrl = document.getElementById('audioControl')
+
+
 canvasContext.fillStyle = "lightgrey";
 
 for(let i = 0; i <1000;i += 10){
     canvasContext.fillRect(i, 0, 5,100);
 }
+
+
 
 let count = 0;
 let percent = 0;
@@ -20,16 +26,14 @@ canvasContext.fillStyle = "orange";
 
 function draw(){
     canvasContext.fillRect(count,0,5,100);
-    //console.log(canvasContext)
+
     count += 10;
     if(count > 100/1){
         return;
-    }
+    }  
     //window.requestAnimationFrame(draw);
 }
 
-var audioEl = document.getElementById("audio")
-var ctrl = document.getElementById('audioControl')
 
 
 audioEl.addEventListener('loadedmetadata', function() {
@@ -57,4 +61,34 @@ audioEl.addEventListener('loadedmetadata', function() {
     audioEl[method]()
   }
   
+  // canvas.addEventListener('click', function() {
+  //   var currentTime =Math.floor(audioEl.duration - audioEl.currentTime) 
+
+  //   updateBar(currentTime)
+
+  //     console.log(currentTime)
+
+  //       //console.log(canvasContext.moveTo(0,currentTime))
+
+  // })
+
+  // Updating the Bar based on current time
+  function updateBar() {
+
+    var currentTime = audioEl.currentTime
+    var duration = audioEl.duration
+
+    //let current = Math.floor(duration - currentTime)
+
+    draw();
+
+    if (currentTime === duration) {
+      ctrl.innerHTML = "Play"
+    }
+    document.getElementById("current-time").innerHTML = convertElapsedTime(currentTime)
+
+    console.log(canvasContext)
+  }
+
+
  
